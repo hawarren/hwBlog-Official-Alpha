d9fba9d25fda93550bc83d69049174f678c75d65
Hanif <Hanif>
2017-03-08 17:11:35 -0500

    fixed passwordreset

--- hwBlog/Controllers/AccountController.cs
+++ hwBlog/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace hwBlog.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email, FirstName = model.FirstName, LastName = model.LastName, DisplayName = model.DisplayName };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
@@ -203,11 +203,12 @@ namespace hwBlog.Controllers
             if (ModelState.IsValid)
             {
                 var user = await UserManager.FindByNameAsync(model.Email);
-                //if (user == null || !(await UserManager.IsEmailConfirmedAsync(user.Id)))
-                //{
-                //    // Don't reveal that the user does not exist or is not confirmed
-                //    return View("ForgotPasswordConfirmation");
-                //}
+                if (user == null)
+				//!(await UserManager.IsEmailConfirmedAsync(user.Id))
+				{
+                    // Don't reveal that the user does not exist or is not confirmed
+                    return View("ForgotPasswordConfirmation");
+                }
 
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                 // Send an email with this link
--- hwBlog/Models/AccountViewModels.cs
+++ hwBlog/Models/AccountViewModels.cs
@@ -65,11 +65,20 @@ namespace hwBlog.Models
     public class RegisterViewModel
     {
         [Required]
-        [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+		[EmailAddress]
+		[Display(Name = "Email")]
+		public string Email { get; set; }
 
-        [Required]
+		[Display(Name = "First Name")]
+		public string FirstName { get; set; }
+
+		[Display(Name = "Last Name")]
+		public string LastName { get; set; }
+
+		[Display(Name = "Display Name")]
+		public string DisplayName { get; set; }
+
+		[Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
